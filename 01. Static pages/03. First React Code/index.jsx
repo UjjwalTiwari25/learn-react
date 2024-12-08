c76e96b075f806a7a54e2b7d5ea827cf8edd667e
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-12-08 16:15:36 +0530

    react

--- 01. Static pages/03. First React Code/index.jsx
+++ 01. Static pages/03. First React Code/index.jsx
@@ -1,4 +1,42 @@
-// Challenge: Re-write the first lines of React code we just had.
-// You can render anything you want to the screen, doesn't have
-// to be an h1 element.
+import { createRoot } from "react-dom/client"
+
+createRoot(document.getElementById("root")).render(
+    
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>City</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>John Doe</td>
+                <td>25</td>
+                <td>New York</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Jane Smith</td>
+                <td>30</td>
+                <td>Los Angeles</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Mike Brown</td>
+                <td>28</td>
+                <td>Chicago</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>Sara Wilson</td>
+                <td>22</td>
+                <td>Houston</td>
+            </tr>
+        </tbody>
+    </table>
+)
 
